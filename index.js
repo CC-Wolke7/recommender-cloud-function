17db22c3de65b3d9b67183b9a357a880b23112e1
Savina Diez <savina.diez@aoe.com>
2021-01-08 11:28:23 +0100

    implement recommender function (local)

--- index.js
+++ index.js
@@ -15,12 +15,41 @@
     gcloud pubsub topics publish MY_TOPIC --message '{ "race": "golden_retriever" }'
 **/
 
+var mysql = require('mysql');
+
 exports.recommend = (_ignored, data) => {
   const msg = data.message;
+  // if base64 encoded:
   // const race = msg.race ? Buffer.from(msg.race, 'base64').toString() : 'None';
-  const race = msg.race ? msg.race : 'None';
 
-  console.log(`Getting ${race} user list ..`)
+  // else:
+  const race = msg.race;
+  if (race === NULL) throw Error('No race provided.');
 
-  // TODO sql call
+  const sql_query = `SELECT user_list FROM race_user_list WHERE race = "${race}"`;
+
+  // in mysql, do: "CREATE VIEW `race_user_list` AS SELECT race, GROUP_CONCAT(user_id) FROM subscription GROUP BY race"
+
+  // connect to db
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "rootybooty",
+    database: "wolkesieben_local"
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected to DB.');
+    con.query(sql_query, function (err, result) {
+      if (err) throw err;
+
+      const user_list = JSON.parse("[" + result[0].user_list + "]");
+      // console.log(user_list);
+
+      // TODO send a message to all user_ids in user_list ..
+
+  });
+});
+
 };
