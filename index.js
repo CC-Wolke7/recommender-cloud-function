4e33c8b6f3ac3782d0c3df2f6da6a94e43b379dd
Savina Diez <savina.diez@aoe.com>
2021-01-17 16:20:24 +0100

    update

--- index.js
+++ index.js
@@ -1,67 +1,81 @@
 const mysql = require('mysql');
 const axios = require('axios');
-const http = require('http');
-const mockserver = require('mockserver');
+const qs = require('qs')
 
-const BOT_ID = 999;
-const baseURL = 'http://localhost:9001';
-
-http.createServer(mockserver('_mocks/')).listen(9001);
+const BOT_UUID = '7c8a6ec3-fc0f-4e0f-95ac-e25f3500c157'
+const BOT_SERVICE_TOKEN = 'NTZiYWU4YjE1ZmQyYzdlMGViZDI1Y2EzODMzZTUxZjQK'
+const baseURL = 'http://localhost:3000';
 
 exports.recommend = (_ignored, data) => {
-  const msg = data.message;
-  // if base64 encoded:
-  // const race = msg.race ? Buffer.from(msg.race, 'base64').toString() : 'None';
+    const msg = data.message;
+    // if base64 encoded: const race = msg.race ? Buffer.from(msg.race, 'base64').toString() : 'None';
 
-  // else:
-  const race = msg.race;
-  const sql_query = `SELECT user_list FROM race_user_list WHERE race = "${race}"`;
+    const breed = msg.breed;
+    const sql_query = `SELECT user_list FROM race_user_list WHERE race = "${breed}"`;
 
-  // connect to db
-  const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "rootybooty",
-    database: "wolkesieben_local"
-  });
-
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log('Connected to DB.');
-    con.query(sql_query, function (err, result) {
-      if (err) throw err;
-
-      const user_list = JSON.parse("[" + result[0].user_list + "]");
-      console.log('Sending messages to users ' + user_list);
-
-      // send message to each user in user list
-      user_list.forEach((user_id) => {
-        const chatID = BOT_ID + ',' + user_id;
-
-        // if bot and user have chatted before, just send message
-        axios.get(baseURL + '/chats/?participants=' + chatID)
-          .then(function (response) {
-            // TODO header Authorization: Bearer <serviceToken>
-            axios.post(baseURL + '/chats/:' + chatID + '/messages/', {message: 'See this cute new ' + race})
-              .then(function (response) {
-                console.log('Message has been sent to user ' + user_id);
-            }).catch(function (error) {
-                console.log(error);
-            });
-          })
-            // if they have never chatted before (chat is not found), send post request to /chats/
-          .catch(function (error) {
-            if (error.response.status === 404){
-              console.log('No chat has been found for chatID ' + chatID + '. Will now instantiate chat');
-              axios.post(baseURL + '/chats/', {participants: chatID})
-                  .then(function (response) {
-                    console.log('Message has been sent to user ' + user_id);
-                  }).catch(function (error) {
-                    console.log(error);
-              });
-            }
-        });
-      });
+    const con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "rootybooty",
+        database: "wolkesieben_local"
     });
-  });
+
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log('Connected to DB.');
+        con.query(sql_query, function (err, result) {
+            if (err) throw err;
+
+            // const user_list = JSON.parse("[" + result[0].user_list + "]");
+            const user_uuids = ['4f975628-d872-4c49-86e7-a176aaf27f7b'];
+
+            user_uuids.forEach((user_uuid) => {
+                const participants = [BOT_UUID, user_uuid];
+
+                const participants_query = qs.stringify({
+                    participants: participants
+                }, {arrayFormat: 'indices'});
+
+
+
+                axios.get(baseURL + '/chats/' + participants_query,
+                    {headers: {Authorization: 'Bearer ' + BOT_SERVICE_TOKEN}})
+
+                    .then(function (response) {
+                        console.log(response);
+                        let chat_id;
+
+                        if (response.data) {
+                            chat_id = response.data['uuid'];
+
+                        } else {
+                            console.log('No chat with bot has been found for user with uuid ' + user_uuid + '. Will now instantiate chat');
+
+                            axios.post(baseURL + '/chats/', {participants: participants_query})
+                                .then(function (response) {
+                                    chat_id = response.data
+                                }).catch(function (error) {
+                                console.log(error);
+                            });
+
+                        }
+
+                        axios.post(baseURL + '/chats/' + chat_id + '/messages/', {message: 'See this cute new ' + breed})
+                                .then(function (response) {
+                                    console.log('Message has been sent to user ' + user_uuid);
+                                }).catch(function (error) {
+                                console.log(error);
+                            });
+
+
+                    })
+                    .catch(function (error) {
+                        console.error("Error response:");
+                        console.error(error.response.data);
+                        console.error(error.response.status);
+                        console.error(error.response.headers);
+                    });
+            });
+        });
+    });
 };
