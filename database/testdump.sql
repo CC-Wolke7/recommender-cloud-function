e479a2702e7828bfd24945f411cd75d869e60427
Savina Diez <savina.diez@aoe.com>
2021-01-27 11:44:10 +0100

    update dump

--- database/testdump.sql
+++ database/testdump.sql
@@ -16,15 +16,15 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Temporary view structure for view `race_user_list`
+-- Temporary view structure for view `breed_user_list`
 --
 
-DROP TABLE IF EXISTS `race_user_list`;
-/*!50001 DROP VIEW IF EXISTS `race_user_list`*/;
+DROP TABLE IF EXISTS `breed_user_list`;
+/*!50001 DROP VIEW IF EXISTS `breed_user_list`*/;
 SET @saved_cs_client     = @@character_set_client;
 /*!50503 SET character_set_client = utf8mb4 */;
-/*!50001 CREATE VIEW `race_user_list` AS SELECT 
- 1 AS `race`,
+/*!50001 CREATE VIEW `breed_user_list` AS SELECT 
+ 1 AS `breed`,
  1 AS `user_list`*/;
 SET character_set_client = @saved_cs_client;
 
@@ -36,10 +36,10 @@ DROP TABLE IF EXISTS `subscription`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `subscription` (
-  `user_id` int NOT NULL,
-  `race` varchar(80) NOT NULL,
-  PRIMARY KEY (`user_id`,`race`),
-  CONSTRAINT `subscription_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  `user_uuid` varchar(36) NOT NULL,
+  `breed` varchar(80) NOT NULL,
+  PRIMARY KEY (`user_uuid`,`breed`),
+  CONSTRAINT `subscription_ibfk_1` FOREIGN KEY (`user_uuid`) REFERENCES `user` (`user_uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -49,7 +49,7 @@ CREATE TABLE `subscription` (
 
 LOCK TABLES `subscription` WRITE;
 /*!40000 ALTER TABLE `subscription` DISABLE KEYS */;
-INSERT INTO `subscription` VALUES (1,'golden_retriever'),(1,'schaeferhund'),(2,'schaeferhund'),(3,'golden_retriever');
+INSERT INTO `subscription` VALUES ('16653104-cca6-4715-b6eb-2ce38df23e69','golden_retriever'),('1f3490a1-7d2d-4ebb-b13b-ef5001330069','schaeferhund'),('f48a1b33-3b3a-49a7-8b94-6cbce697a36f','golden_retriever');
 /*!40000 ALTER TABLE `subscription` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -61,11 +61,11 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(80) NOT NULL,
-  `last_name` varchar(80) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `user_uuid` varchar(36) NOT NULL,
+  `first_name` varchar(80) DEFAULT NULL,
+  `last_name` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`user_uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -74,15 +74,15 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Tina','Tierfreund'),(2,'Hans','Hundemann'),(3,'Vogella','Vogelmensch');
+INSERT INTO `user` VALUES ('16653104-cca6-4715-b6eb-2ce38df23e69','Tina','Tierfreund'),('1f3490a1-7d2d-4ebb-b13b-ef5001330069','Florian','Fischkopf'),('9db3017b-aef1-42d3-a022-9966656cbd8e','Hans','Hundefan'),('f337d6d9-343b-4418-8974-86b452405805','Karina','Katzenfrau'),('f48a1b33-3b3a-49a7-8b94-6cbce697a36f','Vogella','Vogelmensch');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Final view structure for view `race_user_list`
+-- Final view structure for view `breed_user_list`
 --
 
-/*!50001 DROP VIEW IF EXISTS `race_user_list`*/;
+/*!50001 DROP VIEW IF EXISTS `breed_user_list`*/;
 /*!50001 SET @saved_cs_client          = @@character_set_client */;
 /*!50001 SET @saved_cs_results         = @@character_set_results */;
 /*!50001 SET @saved_col_connection     = @@collation_connection */;
@@ -91,7 +91,7 @@ UNLOCK TABLES;
 /*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `race_user_list` AS select `subscription`.`race` AS `race`,group_concat(`subscription`.`user_id` separator ',') AS `user_list` from `subscription` group by `subscription`.`race` */;
+/*!50001 VIEW `breed_user_list` AS select `subscription`.`breed` AS `breed`,group_concat(`subscription`.`user_uuid` separator ',') AS `user_list` from `subscription` group by `subscription`.`breed` */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
@@ -105,4 +105,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-08 11:53:52
+-- Dump completed on 2021-01-27 11:41:09
